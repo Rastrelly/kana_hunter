1a4b95bc7ef737e309e252c7c0e388531d5dfd75
Rastrelly <rastrelly@gmail.com>
2022-11-08 20:46:27 +0200

    upgraded selector
    better new symbol selection, answer selection, UI update

--- ukanahunter.pas
+++ ukanahunter.pas
@@ -23,14 +23,24 @@ type
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
+    but_ans_0: TButton;
+    but_ans_1: TButton;
+    but_ans_2: TButton;
+    but_ans_3: TButton;
+    but_ans_4: TButton;
+    but_ans_5: TButton;
     CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    CheckBox3: TCheckBox;
     Edit1: TEdit;
     gbField: TGroupBox;
     gbSettings: TGroupBox;
     GroupBox1: TGroupBox;
     GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
     Label1: TLabel;
     Label10: TLabel;
+    Label11: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
@@ -42,16 +52,31 @@ type
     ListBox1: TListBox;
     Panel1: TPanel;
     Panel2: TPanel;
+    Panel3: TPanel;
     ProgressBar1: TProgressBar;
     RadioButton1: TRadioButton;
     RadioButton2: TRadioButton;
     RadioButton3: TRadioButton;
+    RadioButton4: TRadioButton;
+    RadioButton5: TRadioButton;
     TrackBar1: TTrackBar;
     ValueListEditor1: TValueListEditor;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure but_ans_0Click(Sender: TObject);
+    procedure but_ans_1Click(Sender: TObject);
+    procedure but_ans_2Click(Sender: TObject);
+    procedure but_ans_3Click(Sender: TObject);
+    procedure but_ans_4Click(Sender: TObject);
+    procedure but_ans_5Click(Sender: TObject);
+    procedure CheckBox3Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure GroupBox3Click(Sender: TObject);
+    procedure ListBox1Click(Sender: TObject);
+    procedure RadioButton4Change(Sender: TObject);
+    procedure refreshProgressBar;
     procedure loadsymboltables;
     procedure showSymbol(sym:integer);
     procedure printlatestsymbol(ls:smb);
@@ -60,6 +85,8 @@ type
     procedure TrackBar1Change(Sender: TObject);
     procedure buildcheatsheet;
     procedure togglecheatsheet;
+    procedure refreshAnsButtons;
+    procedure updateAnsStyle;
   private
 
   public
@@ -76,6 +103,9 @@ var
   gamelevel:integer=0;
 
   answerset:array[0..5] of string;
+
+  newsymbols:array of integer;
+
   correct:smb;
   score:integer=0;
   totscore:integer=0;
@@ -88,12 +118,24 @@ var
 
   sym_tempo:integer=2;
 
+  prev_symb_id:integer=-1;
+
+  sel_answ:integer=0;
+
 
 implementation
 
 {$R *.lfm}
 
 { TForm1 }
+
+uses ukhnewsymbols;
+
+procedure recordToNewsymbols(id:integer);
+begin
+  setlength(newsymbols,length(newsymbols)+1);
+  newsymbols[high(newsymbols)]:=id;
+end;
 
 procedure genactivesdb;
 var i:integer;
@@ -105,6 +147,42 @@ begin
     begin
       SetLength(activesdb,Length(activesdb)+1);
       activesdb[high(activesdb)]:=i;
+    end;
+  end;
+end;
+
+procedure unclockSymbolByName(sym:string);
+var i:integer;
+    expcat1,expcat2:integer;
+begin
+
+  if exptp=1 then
+  begin
+    expcat1:=0;
+    expcat2:=-1;
+  end;
+
+  if exptp=2 then
+  begin
+    expcat1:=1;
+    expcat2:=-1;
+  end;
+
+  if exptp=3 then
+  begin
+    expcat1:=0;
+    expcat2:=1;
+  end;
+
+  if length(symboldb)>0 then
+  for i:=0 to length(symboldb)-1 do
+  begin
+    if symboldb[i].written=sym then
+    if (symboldb[i].category=expcat1) or (symboldb[i].category=expcat2) then
+    begin
+      symboldb[i].active:=true;
+      recordToNewsymbols(i);
+      inc(gamelevel);
     end;
   end;
 end;
@@ -141,7 +219,9 @@ begin
   begin
     symboldb[i].active:=true;
     Form1.printlatestsymbol(symboldb[i]);
+    recordToNewsymbols(i);
     inc(gamelevel);
+    Form2.Show;
   end;
 end;
 
@@ -172,6 +252,14 @@ begin
   result:=tsmb;
 end;
 
+
+procedure tform1.refreshProgressBar;
+begin
+  ProgressBar1.Min:=0;
+  ProgressBar1.Position:=score;
+  ProgressBar1.Max:=gamelevel*sym_tempo;
+end;
+
 procedure tform1.makequestion;
 var i,j,mx,k:integer;
     qid,av,rav:integer;
@@ -179,41 +267,90 @@ var i,j,mx,k:integer;
     canexit:boolean;
     ts:string;
     gp:integer;
+    adbl:integer;
+    tries:integer;
 begin
   genactivesdb;
+  adbl:=length(activesdb);
   mx:=Length(symboldb);
-  qid:=activesdb[random(length(activesdb))];
+  qid:=activesdb[random(adbl)];
+
+  if length(activesdb)>1 then
+  if (qid=prev_symb_id) then
+  while(qid=prev_symb_id) do
+  begin
+    qid:=activesdb[random(adbl)];
+  end;
+
+  prev_symb_id:=qid;
+
   rav:=random(6);
   qs:=symboldb[qid];
   correct:=qs;
   canexit:=false;
+
+  tries:=0;
+
   for i:=0 to 5 do answerset[i]:='@';
+
   for i:=0 to 5 do
   begin
     canexit:=false;
+
     if(i=rav) then answerset[i]:=correct.written;
+
     if(i<>rav) then
     begin
+
       while(not canexit) do
       begin
+
         canexit:=true;
-        k:=random(mx);
-        ts:=symboldb[k].written;
+
+        if (adbl<2) or (tries>1000) then
+        begin
+          k:=random(mx);
+          ts:=symboldb[k].written;
+        end
+        else
+        begin
+          k:=random(adbl);
+          ts:=symboldb[activesdb[k]].written;
+        end;
+
         for j:=0 to 5 do
         begin
           if(ts=answerset[j]) then canexit:=false;
         end;
+
         if (ts = correct.written) then canexit:=false;
+
+        inc(tries);
+
       end;
       answerset[i]:=ts;
     end;
+
   end;
 
   ListBox1.Clear;
+
   for i:=0 to 5 do
   begin
     ListBox1.Items.Add(answerset[i]);
   end;
+
+  but_ans_0.Caption:=answerset[0];
+  but_ans_1.Caption:=answerset[1];
+  but_ans_2.Caption:=answerset[2];
+  but_ans_3.Caption:=answerset[3];
+  but_ans_4.Caption:=answerset[4];
+  but_ans_5.Caption:=answerset[5];
+
+  sel_answ:=-1;
+
+  refreshAnsButtons;
+
   Label2.Caption:=correct.symb;
   Label3.Caption:='Level '+inttostr(gamelevel);
 
@@ -223,7 +360,7 @@ begin
   if (gp<=60) then Label6.Font.Color:=clRed;
   if (gp>60) then Label6.Font.Color:=RGBToColor(255,137,39);
   if (gp>80) then Label6.Font.Color:=clYellow;
-  if (gp>90) then Label6.Font.Color:=clGreen;
+  if (gp>90) then Label6.Font.Color:=clLime;
 
 
   Label6.Caption:='Total score: '+inttostr(totscore) + ' ('+inttostr(r)+'/'+inttostr(w)+': '+inttostr(gp)+'%)';
@@ -234,6 +371,7 @@ end;
 procedure TForm1.TrackBar1Change(Sender: TObject);
 begin
   sym_tempo:=TrackBar1.Position;
+  refreshProgressBar;
 end;
 
 procedure tform1.printlatestsymbol(ls:smb);
@@ -275,6 +413,27 @@ begin
   randomize;
   loadsymboltables;
   togglecheatsheet;
+  updateAnsStyle;
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  refreshAnsButtons;
+end;
+
+procedure TForm1.GroupBox3Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.ListBox1Click(Sender: TObject);
+begin
+  sel_answ:=ListBox1.ItemIndex;
+end;
+
+procedure TForm1.RadioButton4Change(Sender: TObject);
+begin
+  UpdateAnsStyle;
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -300,10 +459,22 @@ begin
 
   if (n<1) then n:=1;
 
-  for i:=0 to n-1 do
+  if (CheckBox2.Checked) then
+  begin
+    unclockSymbolByName('a');
+    unclockSymbolByName('i');
+    unclockSymbolByName('u');
+    unclockSymbolByName('e');
+    unclockSymbolByName('o');
+  end;
+
+  if (gamelevel<(n-1)) then
+  for i:=gamelevel to n-1 do
   begin
     unlockNewSymbol(CheckBox1.Checked);
   end;
+
+  Form2.Show;
 
   makequestion;
 end;
@@ -311,9 +482,10 @@ end;
 procedure TForm1.Button3Click(Sender: TObject);
 var seltext:string;
 begin
-  if (ListBox1.ItemIndex<>-1) then
+  form2.Close;
+  if (sel_answ<>-1) then
   begin
-    seltext:=ListBox1.Items[ListBox1.ItemIndex];
+    seltext:=answerset[sel_answ];
     if(seltext=correct.written) then
     begin
        inc(score);
@@ -323,9 +495,7 @@ begin
          unlockNewSymbol(CheckBox1.Checked);
          score:=0;
        end;
-       ProgressBar1.Min:=0;
-       ProgressBar1.Max:=gamelevel;
-       ProgressBar1.Position:=score;
+       refreshProgressBar;
        Label7.Caption:='Correct! It was '+correct.written+' ('+correct.symb+')';
        Label7.Font.Color:=clGreen;
        inc(r);
@@ -343,6 +513,47 @@ begin
     end;
     makequestion;
   end;
+end;
+
+procedure TForm1.but_ans_0Click(Sender: TObject);
+begin
+  sel_answ:=0;
+  refreshAnsButtons;
+end;
+
+procedure TForm1.but_ans_1Click(Sender: TObject);
+begin
+  sel_answ:=1;
+  refreshAnsButtons;
+end;
+
+procedure TForm1.but_ans_2Click(Sender: TObject);
+begin
+  sel_answ:=2;
+  refreshAnsButtons;
+end;
+
+procedure TForm1.but_ans_3Click(Sender: TObject);
+begin
+  sel_answ:=3;
+  refreshAnsButtons;
+end;
+
+procedure TForm1.but_ans_4Click(Sender: TObject);
+begin
+  sel_answ:=4;
+  refreshAnsButtons;
+end;
+
+procedure TForm1.but_ans_5Click(Sender: TObject);
+begin
+  sel_answ:=5;
+  refreshAnsButtons;
+end;
+
+procedure TForm1.CheckBox3Change(Sender: TObject);
+begin
+  Label4.Visible:=CheckBox3.Checked;
 end;
 
 procedure Tform1.showSymbol(sym:integer);
@@ -383,6 +594,46 @@ begin
   begin
     GroupBox2.Visible:=true;
     Form1.ClientWidth:=Form1.ClientWidth+GroupBox2.Width;
+  end;
+end;
+
+
+procedure tForm1.refreshAnsButtons;
+var bw:integer;
+begin
+  bw:=round(Panel3.Width/6);
+  but_ans_0.Width:=bw;
+  but_ans_1.Width:=bw;
+  but_ans_2.Width:=bw;
+  but_ans_3.Width:=bw;
+  but_ans_4.Width:=bw;
+  but_ans_5.Width:=bw;
+  but_ans_0.Font.Style:=[];
+  but_ans_1.Font.Style:=[];
+  but_ans_2.Font.Style:=[];
+  but_ans_3.Font.Style:=[];
+  but_ans_4.Font.Style:=[];
+  but_ans_5.Font.Style:=[];
+  if(sel_answ=0) then but_ans_0.Font.Style:=[fsBold];
+  if(sel_answ=1) then but_ans_1.Font.Style:=[fsBold];
+  if(sel_answ=2) then but_ans_2.Font.Style:=[fsBold];
+  if(sel_answ=3) then but_ans_3.Font.Style:=[fsBold];
+  if(sel_answ=4) then but_ans_4.Font.Style:=[fsBold];
+  if(sel_answ=5) then but_ans_5.Font.Style:=[fsBold];
+
+end;
+
+procedure tform1.updateAnsStyle;
+begin
+  if(RadioButton4.Checked) then
+  begin
+    Panel3.Visible:=true;
+    Panel2.Visible:=false;
+  end;
+  if(RadioButton5.Checked) then
+  begin
+    Panel3.Visible:=false;
+    Panel2.Visible:=true;
   end;
 end;
 
---
+++ ukhnewsymbols.pas
@@ -0,0 +1,72 @@
+unit ukhnewsymbols;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ukanahunter;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    ListBox1: TListBox;
+    procedure Button1Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+function composesymbol(id:integer):string;
+var med:string;
+begin
+  med:='';
+  if(id>=0) and (id<Length(symboldb)) then
+  begin
+    med:=symboldb[id].symb+' - '+symboldb[id].written;
+    if (symboldb[id].category=0) then med:=med+' (hiragana)';
+    if (symboldb[id].category=1) then med:=med+' (katakana)';
+  end;
+  Result:=med;
+end;
+
+{ TForm2 }
+
+procedure TForm2.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  setlength(newsymbols,0);
+end;
+
+procedure TForm2.FormShow(Sender: TObject);
+var i:integer;
+begin
+  ListBox1.Clear;
+  if length(newsymbols)>0 then
+  for i:=0 to length(newsymbols)-1 do
+  begin
+    ListBox1.Items.Add(composesymbol(newsymbols[i]));
+  end;
+end;
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  setlength(newsymbols,0);
+  Form2.Close;
+end;
+
+end.
+
